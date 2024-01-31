705c8d95d0a7de7f9977cc9100d510942d310593
Matheus Guilherme <matheusguilhermediresto10@gmail.com>
2024-01-31 12:59:12 -0300

    Create apv.java

---
+++ src/apv.java
@@ -0,0 +1,44 @@
+class SinglyLinkedListNode {
+    int data;
+    SinglyLinkedListNode next;
+
+    public SinglyLinkedListNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Solution {
+    static boolean hasCycle(SinglyLinkedListNode head) {
+        if (head == null || head.next == null) {
+            return false; 
+        }
+
+        SinglyLinkedListNode tortoise = head;
+        SinglyLinkedListNode hare = head;
+
+        while (hare != null && hare.next != null) {
+            tortoise = tortoise.next;
+            hare = hare.next.next;
+
+            if (tortoise == hare) {
+                return true; 
+        }
+
+        return false; 
+    }
+
+   
+    public static void main(String[] args) {
+        SinglyLinkedListNode node1 = new SinglyLinkedListNode(1);
+        SinglyLinkedListNode node2 = new SinglyLinkedListNode(2);
+        SinglyLinkedListNode node3 = new SinglyLinkedListNode(3);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node1; 
+
+        boolean result = hasCycle(node1);
+        System.out.println(result); 
+    }
+}
